5de9b4eca4e00179cdf22c7454f7bc31ee99a9a5
Melissa Lowe <mel.c.lowe@gmail.com>
2021-04-26 15:43:19 -0500

    Setting up svg area and transforming to center chart

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,28 @@
 // @TODO: YOUR CODE HERE!
+console.log("app.js loaded")
+
+// Set up chart area
+var svgWidth = 960;
+var svgheight = 500;
+
+var margin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgheight - margin.top - margin.bottom;
+
+// Create wrapper and append svg to index.html
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgheight);
+
+// Translate to center the chart when it will appear on the page
+var chartGroup = svg.append("g")
+    .attr("transfomr", `translate(${margin.left}, ${margin.top})`);
+
+// Import data from data.csv
